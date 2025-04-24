3b976fb96e78cac8fd349cc7b566f741b54be6aa
Tejalpatel2099 <tejalpatel2099@gmail.com>
2025-04-24 16:32:45 -0700

    Added ProductsPage file in Products folder

---
+++ src/Pages/Products/ProductsPage.cshtml.cs
@@ -0,0 +1,28 @@
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace ContosoCrafts.WebSite.Pages.Products
+{
+    public class ProductsPageModel : PageModel
+    {
+        private readonly ILogger<ProductsPageModel> _logger;
+
+        public ProductsPageModel(ILogger<ProductsPageModel> logger,
+            JsonFileProductService productService)
+        {
+            _logger = logger;
+            ProductService = productService;
+        }
+        public JsonFileProductService ProductService { get; }
+        public IEnumerable<ProductModel> Products { get; private set; }
+
+        public void OnGet()
+        {
+            Products = ProductService.GetProducts();
+        }
+    }
+}
