a08b246cc9734ee6e206a22dcf211e29e66b5933
Lucian Petriuc <lucianptr4@gmail.com>
2025-05-06 15:26:19 -0700

    Added tests of Index.cshtml.Test.cs, JsonFileProductServiceTests.cs and updated TestFixture.cs and TestHelper.cs

---
+++ UnitTests/Pages/Product/Index.cshtml.Tests.cs
@@ -0,0 +1,54 @@
+using RamenRatings.WebSite.Pages;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace UnitTests.Pages.Product
+{
+    /// <summary>
+    /// Unit testing for Index Tests
+    /// </summary>
+    public class IndexTests
+    {
+        // Database MiddleTier
+        #region TestSetup
+        public static IndexModel pageModel;
+        private readonly ILogger<IndexModel> _logger;
+        /// <summary>
+        /// Initialize of Test
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new IndexModel(_logger, TestHelper.ProductService)
+            {
+            };
+        }
+
+        #endregion TestSetup
+        /// <summary>
+        /// Checking whether product user want is there in result or not.
+        /// </summary>
+        #region OnGet
+        [Test]
+        public void OnGet_Valid_Should_Return_Products()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet();
+
+            // Assert 
+            // How many are there?
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            // Are there any in existence?
+            Assert.AreEqual(true, pageModel.Products.ToList().Any());
+        }
+        #endregion OnGet
+    }
+}
---
+++ UnitTests/Pages/Services/JsonFileProductServiceTests.cs
@@ -0,0 +1,21 @@
+
+using System.Linq;
+using RamenRatings.WebSite.Models;
+using NUnit.Framework;
+
+
+namespace UnitTests.Pages.Services
+{
+    public class JsonFileProductServiceTests
+    {
+        #region TestSetup
+
+        [SetUp]
+        public void TestInitialize()
+        {
+        }
+
+        #endregion TestSetup
+    }
+
+}
