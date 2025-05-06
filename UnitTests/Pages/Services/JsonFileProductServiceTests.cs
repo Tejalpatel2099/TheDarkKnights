9e7aa156339a23c6fd5b582e6521d748858d6783
Tejal Patel <tpatel3@seattleu.edu>
2025-05-06 16:39:11 -0700

    Added Unit Test for Add Ratings method in JsonFileProductServiceTests.cs

--- UnitTests/Pages/Services/JsonFileProductServiceTests.cs
+++ UnitTests/Pages/Services/JsonFileProductServiceTests.cs
@@ -16,6 +16,68 @@ namespace UnitTests.Pages.Services
         }
 
         #endregion TestSetup
-    }
 
+        #region AddRating
+        /// <summary>
+        /// REST Get Products data
+        /// POST a valid rating
+        /// Test that the last data that was added was added correctly
+        /// </summary>
+        [Test]
+        public void AddRating_Valid_ProductId_Return_true()
+        {
+            // Arrange
+
+
+
+            // Act
+            // Store the result of the AddRating method (which is being tested)
+            bool validAdd = TestHelper.ProductService.AddRating(30, 4);
+
+            // Assert
+            Assert.AreEqual(true, validAdd);
+
+            // Reset
+            // Delete Dummy Record
+        }
+
+        /// <summary>
+        /// REST POST data that doesn't fit the constraints defined in function
+        /// Test if it Adds
+        /// Returns False because it wont add
+        /// </summary>
+        [Test]
+        public void AddRating_Invalid_Product_ID_Not_Present_Should_Return_False()
+        {
+            // Arrange
+
+            // Act
+            // Store the result of the AddRating method (which is being tested)
+            var result = TestHelper.ProductService.AddRating(123, 5);
+
+            // Assert
+            Assert.AreEqual(false, result);
+        }
+
+        /// <summary>
+        /// REST Get Products data
+        /// POST a new valid rating on existing rated product
+        /// Test that the last data that was added was added correctly
+        /// </summary>
+        [Test]
+        public void AddRating_Valid_ProductId_Adding_Rating_On_Existing_Product_Rating_Should_Return_true()
+        {
+            // Arrange
+
+
+            // Act
+            // Store the result of the AddRating method (which is being tested)
+            bool validAdd = TestHelper.ProductService.AddRating(1, 3);
+
+            // Assert
+            Assert.AreEqual(true, validAdd);
+        }
+
+    }
+    #endregion AddRating
 }
