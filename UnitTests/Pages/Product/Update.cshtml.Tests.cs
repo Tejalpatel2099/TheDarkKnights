562353712442ed545379d92af285c846d0838bd5
Tejal Patel <tpatel3@seattleu.edu>
2025-05-06 19:55:29 -0700

    Added Test cases for Update.cshtml.cs in Update.cshtml.Tests.cs to ensure more than 60% of code coverage.

---
+++ UnitTests/Pages/Product/Update.cshtml.Tests.cs
@@ -0,0 +1,155 @@
+using NUnit.Framework;
+using RamenRatings.WebSite.Pages.Product;
+using RamenRatings.WebSite.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+
+namespace UnitTests.Pages.Product
+{
+    public class UpdateTests
+    {
+        #region TestSetup
+
+        public static UpdateModel pageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            // Initialize the page model with a mock ProductService
+            pageModel = new UpdateModel(TestHelper.ProductService)
+            {
+                PageContext = TestHelper.PageContext,
+                TempData = TestHelper.TempData,
+                Url = TestHelper.UrlHelper
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+
+        [Test]
+        public void OnGet_ValidId_Should_Return_Page()
+        {
+            // Arrange
+            int validId = 1;
+
+            // Act
+            var result = pageModel.OnGet(validId);
+
+            // Assert
+            Assert.IsInstanceOf<PageResult>(result);
+            Assert.IsNotNull(pageModel.Product);
+        }
+
+        [Test]
+        public void OnGet_InvalidId_Should_RedirectToError()
+        {
+            // Arrange
+            int invalidId = 999;
+
+            // Act
+            var result = pageModel.OnGet(invalidId);
+
+            // Assert
+            Assert.IsInstanceOf<RedirectToPageResult>(result);
+            var redirectResult = result as RedirectToPageResult;
+            Assert.AreEqual("../Error", redirectResult.PageName);
+        }
+
+        #endregion OnGet
+        #region OnPost
+
+        [Test]
+        public void OnPost_ValidModel_Should_RedirectToProductsPage()
+        {
+            // Arrange
+            var sample = TestHelper.ProductService.GetProducts().First();
+            pageModel.Product = new ProductModel
+            {
+                Number = sample.Number,
+                Brand = sample.Brand,
+                Style = sample.Style,
+                Variety = "Updated Variety",
+                Country = "Updated Country"
+            };
+
+            // Act
+            var result = pageModel.OnPost() as PageResult;
+
+            //Assert
+            Assert.IsInstanceOf <Microsoft.AspNetCore.Mvc.RazorPages.PageResult> (result);
+            
+        }
+       
+
+        [Test]
+        public void OnPost_InvalidModel_Should_Return_Page()
+        {
+            // Arrange
+            pageModel.ModelState.AddModelError("Brand", "Required");
+
+            // Act
+            var result = pageModel.OnPost();
+
+            // Assert
+            Assert.IsInstanceOf<PageResult>(result);
+        }
+
+        #endregion OnPost
+
+        #region UpdateData
+
+        [Test]
+        public void UpdateData_Should_Update_Fields_Correctly()
+        {
+            // Arrange
+            var original = TestHelper.ProductService.GetProducts().First();
+            pageModel.Product = new ProductModel
+            {
+                Number = original.Number,
+                Brand = "Other",
+                Style = "Other",
+                Variety = "Spicy",
+                Country = "Japan"
+            };
+            pageModel.NewBrand = "New Brand";
+            pageModel.NewStyle = "Dry";
+
+            // Act
+            var updated = pageModel.UpdateData();
+
+            // Assert
+            Assert.AreEqual("New Brand", updated.Brand);
+            Assert.AreEqual("Dry", updated.Style);
+            Assert.AreEqual("Spicy", updated.Variety);
+            Assert.AreEqual("Japan", updated.Country);
+        }
+
+        #endregion UpdateData
+
+        #region SaveData
+
+        [Test]
+        public void SaveData_Should_Update_Json_File()
+        {
+            // Arrange
+            var product = TestHelper.ProductService.GetProducts().First();
+            product.Brand = "Test Brand";
+
+            // Act
+            pageModel.SaveData(product);
+
+            // Reload and verify
+            var reloaded = TestHelper.ProductService.GetProducts().First(p => p.Number == product.Number);
+            Assert.AreEqual("Test Brand", reloaded.Brand);
+        }
+
+        #endregion SaveData
+    }
+}
