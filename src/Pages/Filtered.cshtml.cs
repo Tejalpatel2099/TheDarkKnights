7756a7de21a6ded38c3b0b795ef5e38d81ae9436
hemasrigadiraju <gadirajuhemasri@gmail.com>
2025-05-11 16:43:37 -0700

    Created Filtered.cshtml for filter the brand and style

---
+++ src/Pages/Filtered.cshtml.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RamenRatings.WebSite.Models;
+using RamenRatings.WebSite.Services;
+
+namespace RamenRatings.WebSite.Pages
+{
+    public class FilteredModel : PageModel
+    {
+        private readonly JsonFileProductService ProductService;
+
+        public FilteredModel(JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+
+        public IEnumerable<ProductModel> Products { get; set; }
+
+        public List<string> AllBrands { get; set; }
+        public List<string> AllStyles { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string SelectedStyle { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public List<string> SelectedBrands { get; set; }
+
+        public void OnGet()
+        {
+            var products = ProductService.GetProducts();
+
+            AllBrands = products.Select(p => p.Brand).Where(b => !string.IsNullOrEmpty(b)).Distinct().OrderBy(b => b).ToList();
+            AllStyles = products.Select(p => p.Style).Where(s => !string.IsNullOrEmpty(s)).Distinct().OrderBy(s => s).ToList();
+
+            if (SelectedBrands?.Any() == true)
+                products = products.Where(p => SelectedBrands.Contains(p.Brand));
+
+            if (!string.IsNullOrEmpty(SelectedStyle))
+                products = products.Where(p => p.Style == SelectedStyle);
+
+            Products = products;
+        }
+    }
+}
