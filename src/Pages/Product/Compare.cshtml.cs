8737a8c3d3643b27d2dcf4100b482d6961380f94
vedat-sq <vting@seattleu.edu>
2025-05-19 12:26:51 -0700

    Added Compare razor page to Compare two ramens: Compare.cshtml, Compare.cshtml.cs

---
+++ src/Pages/Product/Compare.cshtml.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RamenRatings.WebSite.Models;
+using RamenRatings.WebSite.Services;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RamenRatings.WebSite.Pages.Product
+{
+    /// <summary>
+    /// Page model for comparing a ramen product with another ramen product,
+    /// displaying ramen options in a dropdown and displaying information for each ramen on one page.
+    /// </summary>
+    public class CompareModel : PageModel
+    {
+        // Service used to retrieve ramen product data
+        private JsonFileProductService ProductService;
+
+        // Constructor that initializes the product service 
+        public CompareModel(JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+
+        // Creates list of ramen products
+        public List<ProductModel> Products { get; set; }
+
+        // First selected ramen product number for onpost
+        [BindProperty]
+        public int Selected1 { get; set; }
+
+        // Second selected ramen product number for onpost
+        [BindProperty]
+        public int Selected2 { get; set; }
+
+        // Product information for the first ramen product
+        public ProductModel Ramen1 { get; set; }
+
+        // Product information for second ramen product
+        public ProductModel Ramen2 { get; set; }
+
+
+        // handles the get request when the page is being accessed
+        public void OnGet()
+        {
+            Products = ProductService.GetProducts().ToList();
+        }
+
+        // handles the post request when the page is finished being accessed
+        public void OnPost()
+        {
+            Products = ProductService.GetProducts().ToList();
+            Ramen1 = Products.FirstOrDefault(p => p.Number == Selected1);
+            Ramen2 = Products.FirstOrDefault(p => p.Number == Selected2);
+        }
+    }
+}
