39e91a53391a5a0d2c75d6e45c08a4c3f4a46910
vedat-sq <vting@seattleu.edu>
2025-05-20 19:24:17 -0700

    Created unit tests for Compare.cshtml.Tests.cs

---
+++ UnitTests/Pages/Product/Compare.cshtml.Tests.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NUnit.Framework;
+using RamenRatings.WebSite.Models;
+using RamenRatings.WebSite.Pages;
+using RamenRatings.WebSite.Pages.Product;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Pages.Product
+{
+    /// <summary>
+    /// Unit testing for Compare Tests
+    /// </summary>
+    public class CompareTests
+    {
+        // Set up the tests
+        #region TestSetup
+        // Declare the model of the Compare page to be used in unit tests
+        public static CompareModel pageModel;
+
+        [SetUp]
+        /// <summary>
+        /// Initializes mock Compare page model for testing.
+        /// </summary>
+        public void TestInitialize()
+        {
+            // Initialize the page model with a mock ProductService
+            pageModel = new CompareModel(TestHelper.ProductService)
+            {
+                PageContext = TestHelper.PageContext,
+                TempData = TestHelper.TempData,
+                Url = TestHelper.UrlHelper
+            };
+        }
+
+        #endregion TestSetup
+
+        /// <summary>
+        /// Checking whether OnGet functions correctly. 
+        /// </summary>
+        #region OnGet
+
+        /// Checks that ramen products are populated
+        [Test]
+        public void OnGet_Valid_Should_Populate_Products()
+        {
+            // Act - get the page
+            pageModel.OnGet();
+
+            // Assert - make sure the page returns products
+            Assert.IsNotNull(pageModel.Products);
+            Assert.IsTrue(pageModel.Products.Any());
+        }
+
+        #endregion OnGet
+
+        /// <summary>
+        /// Checking whether OnPost functions correctly. 
+        /// </summary>
+        #region OnPost Tests
+
+        /// Checks that the selections for comparing two ramens are valid 
+        [Test]
+        public void OnPost_Valid_Selection_Should_Assign_Ramen1_And_Ramen2()
+        {
+            // Arrange
+            // Create the list of products
+            var products = TestHelper.ProductService.GetProducts().ToList();
+
+            // Assign first as the first index
+            var first = products[0];
+
+            // If there are more than 2 products, assign the second as the 2nd index on the list
+            var second = products[0];
+            if (products.Count > 1)
+            {
+                second = products[1];
+            }
+
+            // Assign the selections
+            pageModel.Selected1 = first.Number;
+            pageModel.Selected2 = second.Number;
+
+            // Act
+            pageModel.OnPost();
+
+            // Assert
+            // Make sure the 2 ramen choices are assigned 
+            Assert.IsNotNull(pageModel.Ramen1);
+            Assert.IsNotNull(pageModel.Ramen2);
+
+            // Check that the products match up with what was assigned
+            Assert.AreEqual(first.Number, pageModel.Ramen1.Number);
+            Assert.AreEqual(second.Number, pageModel.Ramen2.Number);
+        }
+        #endregion OnPost Tests
+    }
+}
