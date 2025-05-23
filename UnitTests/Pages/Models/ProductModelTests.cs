9d5283b6b2144f392b8d2cf1927de2c81bfd84a4
hemasrigadiraju <gadirajuhemasri@gmail.com>
2025-05-23 09:00:31 -0700

    Added ProductModelTests.cs file

---
+++ UnitTests/Pages/Models/ProductModelTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using RamenRatings.WebSite.Models;
+
+namespace RamenRatings.Tests.Models
+{
+    public class ProductModelTests
+    {
+        [Test]
+        public void ToString_ShouldReturnJsonRepresentation()
+        {
+            // Arrange
+            var product = new ProductModel
+            {
+                Number = 42,
+                Brand = "Nissin",
+                img = "image.png",
+                Style = "Bowl",
+                Country = "Japan",
+                Variety = "Chicken",
+                Vegetarian = "No",
+                Ratings = new[] { 3, 4, 5 },
+                Feedback = new[] { "Good", "Tasty" }
+            };
+
+            // Act
+            var json = product.ToString();
+
+            // Assert
+            Assert.IsNotNull(json);
+            Assert.IsTrue(json.Contains("\"Number\":42"));
+            Assert.IsTrue(json.Contains("\"Brand\":\"Nissin\""));
+            Assert.IsTrue(json.Contains("\"img\":\"image.png\""));
+            Assert.IsTrue(json.Contains("\"Style\":\"Bowl\""));
+            Assert.IsTrue(json.Contains("\"Country\":\"Japan\""));
+            Assert.IsTrue(json.Contains("\"Variety\":\"Chicken\""));
+            Assert.IsTrue(json.Contains("\"Vegetarian\":\"No\""));
+            Assert.IsTrue(json.Contains("\"Ratings\":[3,4,5]"));
+            Assert.IsTrue(json.Contains("\"Feedback\":[\"Good\",\"Tasty\"]"));
+        }
+    }
+}
