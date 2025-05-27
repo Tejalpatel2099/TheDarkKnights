856204ec9ad7fafa161d76c42a6688860fec9b48
vedat-sq <vting@seattleu.edu>
2025-05-26 18:54:59 -0700

    Updated pageModel initialization in About.cshtml.Tests.cs

--- UnitTests/Pages/Product/About.cshtml.Tests.cs
+++ UnitTests/Pages/Product/About.cshtml.Tests.cs
@@ -5,17 +5,28 @@ using Moq;
 
 namespace UnitTests.Pages
 {
+    /// <summary>
+    /// Unit tests for the About Razor PageModel
+    /// </summary>
     public class AboutTests
     {
         #region TestSetup
-        public static AboutModel pageModel;
-        
-        [SetUp]
 
+        // pageModel used for testing
+        public static AboutModel pageModel;
+
+        /// <summary>
+        /// Initializes the test by creating an instance of the AboutModel 
+        /// with a mock logger and the real product service
+        /// </summary>
+        [SetUp]
         public void TestInitialize()
         {
             var MockLoggerDirect = Mock.Of<ILogger<AboutModel>>();
-            pageModel = new AboutModel(MockLoggerDirect)
+            var productService = TestHelper.ProductService;
+
+
+            pageModel = new AboutModel(MockLoggerDirect, productService)
             {
                 PageContext = TestHelper.PageContext,
                 TempData = TestHelper.TempData,
@@ -23,8 +34,11 @@ namespace UnitTests.Pages
         }
 
         #endregion TestSetup
-        
+
         #region OnGet
+        /// <summary>
+        /// Verifies that  OnGet initializes the page without model errors
+        /// </summary>
         [Test]
         public void OnGet_Valid_Should_Return_Valid_Page()
         {
